d61b5bc7dc0a52d0ab06ce9d5de52b3670018196
Zhumazhenis Dairabay <zhumazhenis@gmail.com>
2018-04-23 03:56:44 +0600

    Update functions.cpp

--- AssemblerCSA/functions.cpp
+++ AssemblerCSA/functions.cpp
@@ -1,259 +1,260 @@
 //
-// Created by Zhumazhenis on 26/01/2018.
+// Created by Zhumazhenis on 03/03/2018.
 //
+
 #include "functions.h"
+#include <vector>
 
 using namespace std;
 
-/* Defining vairables */
-
-string error;
-char c[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-string s[] = {"NOP", "MOV", "ADD", "SUB", "MUL", "DIV", "INC", "DEC", "AND", "OR",
-              "XOR", "CMP", "CLR", "JMP", "JZ", "JNZ", "CPL", "XCHG", "HLT", ""};
-
-string s2[] = {"", "A,B", "B,A", "A,#X", "B,#X", "A,Address", "B,Address", "Address,A", "Address,B", "Address"};
-
-string keys[] = {
-        "NOP ", "MOV A,B", "MOV B,A", "MOV A,#X", "MOV B,#X", "MOV A,Address", "MOV B,Address", "MOV Address,A",
-        "MOV Address,B", "ADD A,B", "SUB A,B", "MUL A,B", "DIV A,B", "INC A", "INC B", "DEC A", "DEC B", "AND A,B",
-        "OR A,B", "XOR A,B", "CMP A,B", "CLR A", "CLR B", "JMP Address", "JZ Address", "JNZ Address", "CPL A", "CPL B",
-        "XCHG A,B", "HLT "
+int pc = 0;
+int len;
+char a = 0;
+char b = 0;
+int *content;
+bool isArithmetic = false;
+pair<int, int> hexByte[] = {
+        mp(0x00, 1), mp(0x01, 1), mp(0x02, 1), mp(0x03, 2), mp(0x04, 2), mp(0x05, 3),
+        mp(0x06, 3), mp(0x07, 3), mp(0x08, 3), mp(0x09, 1), mp(0x0A, 1), mp(0x0B, 1),
+        mp(0x0C, 1), mp(0x0D, 1), mp(0x0E, 1), mp(0x0F, 1), mp(0x10, 1), mp(0x11, 1),
+        mp(0x12, 1), mp(0x13, 1), mp(0x14, 1), mp(0x15, 1), mp(0x16, 1), mp(0x17, 3),
+        mp(0x18, 3), mp(0x19, 3), mp(0x20, 1), mp(0x21, 1), mp(0x22, 1), mp(0x23, 1)
 };
 
-set<char> addressSet(c, c + sizeof(c) / sizeof(c[0]));
-set<string> mnemSet(s, s + sizeof(s) / sizeof(s[0]));
-set<string> operandSet(s2, s2 + sizeof(s2) / sizeof(s2[0]));
 
-pair<string, int> hexByte[] = {
-        mp("00", 1), mp("01", 1), mp("02", 1), mp("03", 2), mp("04", 2), mp("05", 3),
-        mp("06", 3), mp("07", 3), mp("08", 3), mp("09", 1), mp("0A", 1), mp("0B", 1),
-        mp("0C", 1), mp("0D", 1), mp("0E", 1), mp("0F", 1), mp("10", 1), mp("11", 1),
-        mp("12", 1), mp("13", 1), mp("14", 1), mp("15", 1), mp("16", 1), mp("17", 3),
-        mp("18", 3), mp("19", 3), mp("20", 1), mp("21", 1), mp("22", 1), mp("23", 1)
-};
 
-map<string, pair<string, int>> values;
-map<string, pair<string, int>>::iterator it = values.begin();
+map<int, int> values; // map
+map<int, int>::iterator it = values.begin();
 
+/* Builds map using hexByte array */
 void assignKeys() {
-    for (int i = 0; i < sizeof(keys) / sizeof(*keys); i++) {
-        values.insert(it, pair<string, pair<string, int>>(keys[i], hexByte[i]));
+    for (int i = 0; i < sizeof(hexByte) / sizeof(hexByte[0]); i++) {
+        values.insert(it, pair<int, int>(hexByte[i].first, hexByte[i].second));
     }
 }
 
-///////////////////////////////////////////////////////////////////////
-
-/* Defining functions */
-
-void process() {
-    assignKeys();
-
-    int n;
-    cout << "ASSEMBLY to HEXADECIMAL converter program\n\nEnter the number of files: ";
-    cin >> n;
-    cout << "\n";
-    string fileName[n];
-    for (int i = 0; i < n; i++) {
-        cout << "Enter " << i + 1 << " filename: ";
-        cin >> fileName[i];
-    }
-
-    for (int i = 0; i < n; i++) {
-        toHex(fileName[i]);
-    }
-    cout << "\n\n";
-    system("pause");
-}
-
-string getMnemonic(ifstream &fptr) {
+/* Task 1: Reads from .hex file, and saves them to dynamic array */
+char *getHex(ifstream &fptr) {
     string s;
     fptr >> s;
-    return s;
-}
-
-string getOperand(ifstream &fptr) {
-    string operand = "", temp;
-    while (fptr.peek() == ' ' || fptr.peek() == '\t')
-        fptr.get();
-    while (fptr.peek() != '\n' && !fptr.eof()) {
-        if ((bool) (fptr >> temp)) {
-            if (!operand.empty()) {
-                if (operand.back() != ',' && temp[0] != ',') {
-                    operand.append(" ");
-                }
-            }
-            operand.append(temp);
-        }
-        while (fptr.peek() == ' ' || fptr.peek() == '\t')
-            fptr.get();
+    len = s.length() / 2;
+    if (len == 0)
+        return nullptr;
+    char *ans = new char[len];
+    for (int i = 0; i < len; i++) {
+        ans[i] = (char) stoul(s.substr((unsigned int) (2 * i), 2), nullptr, 16);
     }
-    return operand;
-}
-
-string normOperand(string operand) {
-
-    if (isData(operand)) {
-        for (long long int i = operand.length() - 1; i >= 0; i--) {
-            if (operand[i] != '#') {
-                operand.erase(operand.begin() + i);
-            } else {
-                operand.append("X");
-                break;
-            }
-        }
-    }
-
-    if (isAddress(operand)) {
-        string temp = "Address";
-        operand.erase(0, 4);
-        operand = temp.append(operand);
-    } else if (operand.length() >= 5 &&
-               isAddress(operand.substr(operand.length() - 4, 4))) {
-
-        operand.erase(operand.length() - 4, 4);
-        operand.append("Address");
-    }
-    return operand;
-}
-
-bool isMnemonic(string mnemonic) {
-    return mnemSet.find(mnemonic) != mnemSet.end();
-}
-
-bool isOperand(string operand) {
-    return operandSet.find(operand) != operandSet.end();
-}
-
-bool isAddress(string s) {
-    for (int i = 0; i < 4; i++) {
-        if (!(addressSet.find(s[i]) != addressSet.end()))
-            return false;
-    }
-    return true;
-}
-
-bool isData(string s) {
-    for (int i = (int) s.length() - 1; i >= 0; i--) {
-        if (s[i] == '#') {
-            string a = "";
-            a = s.substr((unsigned int) i + 1, s.length() - 1 - i);
-            if (a.length() > 2) {
-                return false;
-            } else if (a.length() == 2) {
-                return (addressSet.find(a[0]) != addressSet.end()) &&
-                       (addressSet.find(a[1]) != addressSet.end());
-            } else if (a.length() == 1) {
-                return addressSet.find(a[0]) != addressSet.end();
-            }
-
-            return false;
-        }
-    }
-    return false;
-}
-
-string extract(string operand) {
-    string data = "";
-    if (isData(operand)) {
-        for (int i = (int) operand.length() - 1; i >= 0; i--) {
-            if (operand[i] != '#') {
-                string temp = string(1, operand[i]);
-                temp.append(data);
-                data = temp;
-
-            } else {
-                if (data.length() == 1) {
-                    string temp = "0";
-                    temp.append(data);
-                    data = temp;
-                }
-                return data;
-            }
-        }
-    } else if (isAddress(operand.substr(0, 4))) {
-        return operand.substr(0, 4);
-    } else if (operand.length() > 4 &&
-               isAddress(operand.substr(operand.length() - 4, 4))) {
-        return operand.substr(operand.length() - 4, 4);
-    }
-    return "";
-}
-
-void toHex(string inFileName) {
-
-    ifstream fptr;
-    ofstream outFile;
-    string outName = inFileName.substr(0, inFileName.length() - 3);
-    outName.append("hex");
-
-    fptr.open(inFileName);
-    if (!(fptr.is_open())) {
-        cout << "\n\n\"" << inFileName << "\" could not open!";
-        return;
-    }
-
-    string mnemonic, operand, ans = "", copyOperand, temp = "", tempError = "";
-    int line = 1, bytes = 0;
-    while (!fptr.eof()) {
-        mnemonic = getMnemonic(fptr);
-        if (!mnemonic.length()) {
-            break;
-        }
-        copyOperand = getOperand(fptr);
-        operand = normOperand(copyOperand);
-
-        if (!isMnemonic(mnemonic)) {
-            tempError = "There is no such mnemonic ";
-            tempError.append(mnemonic);
-            tempError.append("; ");
-        }
-        if (!isOperand(operand)) {
-            tempError.append("There is no such operand ");
-            tempError.append(copyOperand);
-            tempError.append("; ");
-        }
-
-        temp.append(mnemonic);
-        temp.append(" ");
-        temp.append(operand);
-
-        if (values.count(temp)) {
-            ans.append(values[temp].first);
-            ans.append(extract(copyOperand));
-            bytes += values[temp].second;
-        } else if (tempError.length() == 0) {
-            tempError.append(mnemonic);
-            tempError.append(" mnemonic does not support operand ");
-            tempError.append(copyOperand);
-            tempError.append(";");
-        }
-
-        if (tempError.length() > 0) {
-            temp = to_string(line);
-            temp.append(" line: ");
-            temp.append(tempError);
-            temp.append("\n");
-            tempError = temp;
-        }
-
-        error.append(tempError);
-        tempError = "";
-        temp = "";
-        line++;
-    }
-
     fptr.close();
-    outFile.open(outName, ofstream::out | ofstream::trunc);
+    return ans;
+}
 
-    if (error.length()) {
-        cout << "\n\n\"" << inFileName << "\" has error following line(s):\n" << error;
-        outFile << error;
-    } else {
-        cout << "\n\n\"" << outName << "\" succeeded: " << ans << "; \tnumber of bytes: " << bytes;
-        outFile << ans;
+/* Task 2: ALU function for arithmetic operations only */
+char alu(char a1, char b1, char op) {
+    isArithmetic = true;
+    int ans = 9999;
+    // ADD
+    if (op == 0x09)
+        ans = ((int) a1 + (int) b1);
+
+    // SUB
+    if (op == 0x0A)
+        ans = ((int) a1 - (int) b1);
+
+    // MUL
+    if (op == 0x0B)
+        ans = ((int) a1 * (int) b1);
+
+    // DIV
+    if (op == 0x0C)
+        ans = ((int) a1 / (int) b1);
+
+    // AND
+    if (op == 0x11)
+        ans = a1 & b1;
+
+    // OR
+    if (op == 0x12)
+        ans = a1 | b1;
+
+    // XOR
+    if (op == 0x13)
+        ans = a1 ^ b1;
+
+    // CMP returns maximum, (but NOT sure, how CMP mnemonic works in Assembly Language)
+    if (op == 0x14) {
+        if((int)a1 == (int)b1)
+            ans = 0;
+        else
+            ans = 1;
     }
 
-    outFile.close();
-    error = "";
+    if ((-128 <= ans) && (ans <= 127)) {
+        return (char) ans;
+    }
+
+    isArithmetic = false;
+}
+
+/* Task 3:  Update PC, which increments by offset */
+void updatePC(int offset) {
+    pc += offset;
+}
+
+/* Task 1: Tests getHex() function */
+void testGetHex() {
+    cout << "Task 1:\n";
+    ifstream myFile = buildFile();
+    char *ram = getHex(myFile); // reads from .hex file, and saves them in ram dynamic array (pointer)
+    for (int i = 0; i < len; i++) {
+        cout << setfill('0') << setw(2) << hex << (int) ((unsigned char) ram[i]) << endl;
+    }
+    myFile.close();
+}
+
+/* Task 2: Test alu() function */
+void testAlu() {
+    cout << "\nTask 2:\n";
+    ifstream myFile = buildFile();
+    char *ram = getHex(myFile);
+
+    char a = (char) (0x10);
+    char b = (char) (0x04);
+    cout << "a = 0x" << hex << (unsigned int) a << ", b = 0x" << (unsigned int) b << endl;
+    cout << "Instr: \t" << "Ans:\n";
+
+    for (int i = 0; i < len; i++) {
+        cout << uppercase << hex << setfill('0') << setw(2) << (int) (ram[i]) << " \t";
+        char c = alu(a, b, ram[i]);
+        if (isArithmetic)
+            cout << hex << "0x" << (int) c << endl;
+        else
+            cout << "ERROR!" << endl;
+    }
+    myFile.close();
+}
+
+/* Task 4: Tests Task 4, using getHex() and updatePC() functions */
+
+
+ifstream buildFile() {
+    ifstream myFile;
+    string hexFileName;
+    cout << "Enter hex file name: ";
+    cin >> hexFileName;
+    myFile.open(hexFileName);
+    return myFile;
+}
+
+
+
+
+/* final */
+
+
+
+void testTask34() {
+    assignKeys();
+    cout << "\nTask 3, 4:\n";
+    ifstream myFile = buildFile();
+    char *ram = getHex(myFile); // function from from Task 1
+
+    cout << "Bytes: \t" << "Instr:" << endl;
+    while (pc < len) {
+        int offset = values[(int) ram[pc]];
+        printf("%dB\t", offset);
+        printf("%02x ", (int)ram[pc]);
+
+        if (offset == 3) {
+            // char addr = (char) (((int) ram[pc + 1] * 0x100) + (int) ram[pc + 2]);
+            printf("%02x%02x ", (int) ram[pc + 1], (int) ram[pc + 2]);
+
+        } else if (offset == 2) {
+
+            printf("%02x ", (int) ram[pc + 1]);
+
+        }
+        cout << endl;
+        updatePC(offset); // function from Task 3
+    }
+    myFile.close();
+}
+
+/* Task 4: Tests Task 4, using getHex() and updatePC() functions */
+void finalFunction() {
+    set<int> index;
+    a = b = 0;
+    assignKeys();
+    content = new int[5000];
+    cout << "\nFinal result: \n";
+    ifstream myFile = buildFile();
+    char *ram = getHex(myFile); // function from from Task 1
+
+    cout << "Bytes: \t" << "Instr:\t" << "Reg A:\t" << "Reg B:" << endl;
+    while (pc < len) {
+        int offset = values[(int) ram[pc]];
+        printf("%dB\t", offset);
+        printf("%02x ", (int)ram[pc]);
+
+        if (offset == 3) {
+            // char addr = (char) (((int) ram[pc + 1] * 0x100) + (int) ram[pc + 2]);
+            printf("%02x%02x", (int) ram[pc + 1], (int) ram[pc + 2]);
+
+            switch(ram[pc]) {
+                case 0x05:
+                    a = content[((int)ram[pc + 1]) * 0x100 + (int)ram[pc + 2]];
+                    break;
+                case 0x06:
+                    b = content[((int)ram[pc + 1]) * 0x100 + (int)ram[pc + 2]];
+                    break;
+                case 0x07:
+                    content[((int) ram[pc + 1]) * 0x100 + (int) ram[pc + 2]] = (int) a;
+                    index.insert(((int) ram[pc + 1]) * 0x100 + (int) ram[pc + 2]);
+                    break;
+                case 0x08:
+                    content[((int) ram[pc + 1]) * 0x100 + (int) ram[pc + 2]] = b;
+                    index.insert(((int) ram[pc + 1]) * 0x100 + (int) ram[pc + 2]);
+                    break;
+                case 0x17:
+                    ram[pc + 3] = content[((int)ram[pc + 1]) * 0x100 + (int)ram[pc + 2]];
+                    break;
+                case 0x18:
+                    if(a == 0x00)
+                        ram[pc + 3] = content[((int)ram[pc + 1]) * 0x100 + (int)ram[pc + 2]];
+                    break;
+                case 0x19:
+                    if(a != 0x00)
+                        ram[pc + 3] = content[((int)ram[pc + 1]) * 0x100 + (int)ram[pc + 2]];
+                    break;
+            }
+
+
+        } else if (offset == 2) {
+            switch(ram[pc]) {
+                case 0x03:
+                    a = ram[pc + 1];
+                    break;
+                case 0x04:
+                    b = ram[pc + 1];
+                    break;
+            }
+            printf("%02x", (int) ram[pc + 1]);
+        }
+
+        char temp = alu(a, b, ram[pc]);
+        if(isArithmetic)
+            a = temp;
+        cout << "\t";
+        cout << hex << setfill('0') << setw(2) << (int) ((unsigned char) a);
+        cout << "\t";
+        cout << hex << setfill('0') <<setw(2) << (int) ((unsigned char) b) << " ";
+        cout << endl;
+        updatePC(offset); // function from Task 3
+    }
+
+    cout << endl;
+    for (std::set<int>::iterator it=index.begin(); it!=index.end(); ++it) {
+        printf("%02x address: %02x\n", (int) *it, (int) content[*it]);
+    }
+
+    myFile.close();
 }
