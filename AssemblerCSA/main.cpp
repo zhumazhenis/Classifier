f43abc806dfe1d0588e8c6f52432c0fd99fe489d
Zhumazhenis Dairabay <zhumazhenis@gmail.com>
2018-04-23 03:57:20 +0600

    Update main.cpp

--- AssemblerCSA/main.cpp
+++ AssemblerCSA/main.cpp
@@ -1,6 +1,20 @@
 #include "functions.h"
 
+using namespace std;
+
 int main() {
-    process();
+
+    // Task 1: it separates by 1 byte
+  //  testGetHex();
+
+    //Task 2: alu function is tested
+ //   testAlu();
+
+    // Task 3, 4: properly separate mnemonics and operands
+    finalFunction();
+
     return 0;
 }
+
+
+
